a244903f1c3a3e787e40aed6206eb83979d6a8fd
Josef Jadrny <bc.josefjadrny@gmail.com>
2019-03-05 00:14:22 +0100

    feat: added detecting custom element

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,33 @@ class AdblockTester {
   private testElementId = 'ads-banner';
 
   isAdblockEnabled(): boolean {
-    const testElement = this.generateElementWithId(this.testElementId);
+    return this.isElementWithIdBlocked(this.testElementId);
+  }
+
+  isElementWithIdBlocked(elementId: string): boolean {
+    const testElement = this.generateElementWithId(elementId);
     this.appendElementToBody(testElement);
     const isElementVisible = this.isElementVisible(testElement);
     this.removeElementFromBody(testElement);
     return !isElementVisible;
   }
 
-  generateElementWithId(elementId: string) {
+  private generateElementWithId(elementId: string) {
     const newElement = document.createElement('div');
     newElement.id = elementId;
     return newElement;
   }
 
-  appendElementToBody(element: HTMLElement): HTMLElement {
+  private appendElementToBody(element: HTMLElement): HTMLElement {
     document.body.appendChild(element);
     return element;
   }
 
-  removeElementFromBody(element: HTMLElement) {
+  private removeElementFromBody(element: HTMLElement) {
     document.body.removeChild(element);
   }
 
-  isElementVisible(element: HTMLElement): boolean {
+  private isElementVisible(element: HTMLElement): boolean {
     return element.offsetParent !== null;
   }
 }
