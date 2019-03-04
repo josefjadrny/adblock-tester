68716999764701efc491d88b37cd9e00948bd9d4
Josef Jadrny <bc.josefjadrny@gmail.com>
2019-03-04 23:36:49 +0100

    feat: adblock + adblock plus test is now working

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-const world = '🗺️';
+class AdblockTester {
+  private testElementId = 'ads-banner';
 
-export function hello(word: string = world): string {
-    return `Hello ${world}! `;
+  isAdblockEnabled(): boolean {
+    const testElement = this.generateElementWithId(this.testElementId);
+    this.appendElementToBody(testElement);
+    const isElementVisible = this.isElementVisible(testElement);
+    this.removeElementFromBody(testElement);
+    return !isElementVisible;
+  }
+
+  generateElementWithId(elementId: string) {
+    const newElement = document.createElement('div');
+    newElement.id = elementId;
+    return newElement;
+  }
+
+  appendElementToBody(element: HTMLElement): HTMLElement {
+    document.body.appendChild(element);
+    return element;
+  }
+
+  removeElementFromBody(element: HTMLElement) {
+    document.body.removeChild(element);
+  }
+
+  isElementVisible(element: HTMLElement): boolean {
+    return element.offsetParent !== null;
+  }
 }
+
+const adblockTester = new AdblockTester();
